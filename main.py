a780aae71f5fca27891da8b4f0fe821dc20ae40e
Ty Esser <sharpslinger@users.noreply.github.com>
2023-02-11 18:22:07 -0600

    adding missed files

--- main.py
+++ main.py
@@ -28,16 +28,18 @@ from ldm.util import instantiate_from_config
 def load_model_from_config(config, ckpt, verbose=False):
     print(f"Loading model from {ckpt}")
     pl_sd = torch.load(ckpt, map_location="cpu")
-    sd = pl_sd["state_dict"]
     config.model.params.ckpt_path = ckpt
     model = instantiate_from_config(config.model)
-    m, u = model.load_state_dict(sd, strict=False)
-    if len(m) > 0 and verbose:
-        print("missing keys:")
-        print(m)
-    if len(u) > 0 and verbose:
-        print("unexpected keys:")
-        print(u)
+    if 'state_dict' in pl_sd:
+        sd = pl_sd["state_dict"]
+    
+        m, u = model.load_state_dict(sd, strict=False)
+        if len(m) > 0 and verbose:
+            print("missing keys:")
+            print(m)
+        if len(u) > 0 and verbose:
+            print("unexpected keys:")
+            print(u)
 
     model.cuda()
     return model
